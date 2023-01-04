20d32af4d05be96a686c2161f1cb2a0b18d5f549
IBRA <ifanl.ibrahim@laplateforme.io>
2023-01-04 17:02:20 +0100

    first commit

--- frontend/App.js
+++ frontend/App.js
@@ -1,24 +1,38 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-// import Carousel from 'react-native-reanimated-carousel';
-import PagerView from 'react-native-pager-view';
+import Carousel from 'react-native-reanimated-carousel';
+// import PagerView from 'react-native-pager-view';
 import Home from './component/home.js';
 import GeneralCondition from './component/generalCondition.js';
 import Login from './component/login.js';
 
 const App = () => {
+  const width = Dimensions.get('window').width;
   return (
-    <PagerView initialPage={0} style={{ flex: 1 }}>
-      <View key="1" style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Home />
-      </View>
-      <View key="2" style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <GeneralCondition />
-      </View>
-      <View key="3" style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Login />
-      </View>
-    </PagerView>
+    <View style={{ flex: 1 }}>
+      <Carousel
+        loop
+        width={width}
+        height={width / 2}
+        autoPlay={true}
+        data={[...new Array(6).keys()]}
+        scrollAnimationDuration={1000}
+        onSnapToItem={(index) => console.log('current index:', index)}
+        renderItem={({ index }) => (
+          <View
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              justifyContent: 'center',
+            }}
+          >
+            {index === 0 && <Home />}
+            {index === 1 && <GeneralCondition />}
+            {index === 2 && <Login />}
+          </View>
+        )}
+      />
+    </View>
   );
 };
   
