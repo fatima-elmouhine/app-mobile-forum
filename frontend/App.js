ca74ac70459f2b84f762b313edb96d6e669fcc63
IBRA <ifanl.ibrahim@laplateforme.io>
2023-01-05 17:13:55 +0100

    add animate

--- frontend/App.js
+++ frontend/App.js
@@ -1,37 +1,57 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
-// import PagerView from 'react-native-pager-view';
+import PagerView from 'react-native-pager-view';
+import Animated, { useHandler, useEvent } from 'react-native-reanimated';
 import Home from './component/home.js';
 import GeneralCondition from './component/generalCondition.js';
 import Login from './component/login.js';
+import PaginationSlider from './component/paginationSlider.js';
+
+const AnimatedPager = Animated.createAnimatedComponent(PagerView);
+
+export function usePagerScrollHandler(handlers, dependencies) {
+  const { context, doDependenciesDiffer } = useHandler(handlers, dependencies);
+  const subscribeForEvents = ['onPageScroll'];
+
+  return useEvent(
+    (event) => {
+      'worklet';
+      const { onPageScroll } = handlers;
+      if (onPageScroll && event.eventName.endsWith('onPageScroll')) {
+        onPageScroll(event, context);
+      }
+    },
+    subscribeForEvents,
+    doDependenciesDiffer
+  );
+}
 
 const App = () => {
-  const width = Dimensions.get('window').width;
+  // const [page, setPage] = useState(0);
+
+  const handler = usePagerScrollHandler({
+    onPageScroll: (e) => {
+      'worklet';
+      // setPage(e.position);
+      console.log('logPosition', e.position);
+    },
+  });
+
   return (
     <View style={{ flex: 1 }}>
-      <Carousel
-        loop
-        width={width}
-        height={width / 2}
-        autoPlay={true}
-        data={[...new Array(6).keys()]}
-        scrollAnimationDuration={1000}
-        onSnapToItem={(index) => console.log('current index:', index)}
-        renderItem={({ index }) => (
-          <View
-            style={{
-              flex: 1,
-              borderWidth: 1,
-              justifyContent: 'center',
-            }}
-          >
-            {index === 0 && <Home />}
-            {index === 1 && <GeneralCondition />}
-            {index === 2 && <Login />}
-          </View>
-        )}
-      />
+      <AnimatedPager initialPage={0} style={{ flex: 1 }} onPageScroll={handler}>
+        <View key="1" style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Home />
+        </View>
+        <View key="2" style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <GeneralCondition />
+        </View>
+        <View key="3" style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Login />
+        </View>
+      </AnimatedPager>
+      <PaginationSlider /*page={page}*/ />
     </View>
   );
 };
--- frontend/component/generalCondition.js
+++ frontend/component/generalCondition.js
@@ -1,8 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 
 export default function GeneralCondition() {
   return (
-    <Text>/***** GENERAL CONDITION *****/</Text>
+    <View style={style.container}>
+      <Text style={style.title}>GENERAL CONDITION</Text>
+      <SafeAreaView style={style.safeare}>
+        <ScrollView style={style.scrollview}>
+          <Text style={style.paragraphe}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
+    marginTop: 100,
+  },
+  safeare: {
+    flex: 1,
+    marginHorizontal: 50,
+    marginVertical: 100,
+    marginTop: 5,
+  },
+  scrollview: {
+    flex: 1,
+    marginTop: 20,
+  },
+  paragraphe: {
+    fontSize: 20,
+    color: '#000',
+    marginBottom: 20,
+  },
+});
