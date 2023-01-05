fe2d6fedb7b10f950d07044241cbb08495ac6d32
IBRA <ifanl.ibrahim@laplateforme.io>
2023-01-05 17:13:55 +0100

    add animate

--- frontend/component/home.js
+++ frontend/component/home.js
@@ -1,8 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Home() {
   return (
-    <Text>/***** HOME *****/</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>MED EN PHARMA KINE</Text>
+      <Image source={require('../assets/BMW-Logo-1923-1953.webp')} style={styles.image}/>
+      <Text style={styles.paragraphe}>L'avenir à portée de main</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  paragraphe: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 20,
+  },
+});
--- frontend/component/login.js
+++ frontend/component/login.js
@@ -1,8 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
 
 export default function Login() {
+
+  const handleLogin = () => {
+    console.log('login');
+  }
+
   return (
-    <Text>/***** LOGIN *****/</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>LOGIN</Text>
+      <SafeAreaView style={styles.form}>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Password" />
+        <Button title="Connexion" style={styles.button} onPress={handleLogin}/>
+      </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  form: {
+    width: '100%',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    width: 250
+  },
+  button: {
+    width: '100%',
+    padding: 20,
+  },
+});
