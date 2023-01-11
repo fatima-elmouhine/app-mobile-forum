9586c09cfcd24da6584f134175148585b18b077f
IBRA <ifanl.ibrahim@laplateforme.io>
2023-01-11 09:28:28 +0100

    test

--- frontend/api/config.js
+++ frontend/api/config.js
@@ -1,22 +1,18 @@
 import axios from 'axios'
-import * as SecureStore from 'expo-secure-store';
+// import * as SecureStore from 'expo-secure-store';
 
 const axiosInstance = axios.create({
   baseURL: 'http://10.10.27.154:3000/api/', responseType: 'json',
 })
-// console.log(axiosInstance)
 
 axiosInstance.interceptors.request.use(function (request) {
-  // console.log(request)
-  // console.log(request)
-//   if (localStorage.getItem('authenticationToken')) {
-//     request.headers['Authorization'] = `Bearer ${localStorage.getItem('authenticationToken')}`
-//   }
 
-
+  // if (SecureStore.getItem('authenticationToken')) {
+  //   request.headers['Authorization'] = `Bearer ${SecureStore.getItem('authenticationToken')}`
+  // }
   return request
 }, function (error) {
-  console.log(error)
+  console.log('config', error)
   return Promise.reject(error)
 })
 export default axiosInstance
--- frontend/component/LogoScreen.js
+++ frontend/component/LogoScreen.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-// import style from '../css/Background.module.css';
 
 function LogoScreen() {
   return (
@@ -10,9 +9,9 @@ function LogoScreen() {
         colors={['#000000', '#0075FF']}
         style={styles.linear}
       >
-        <ImageBackground source={require('../assets/logo_fond.png')} resizeMode="cover" style={styles.image_fond}/>
-          <Image source={require('../assets/logoMed.png')} style={styles.image}/>
-          <Text style={styles.paragraphe}>Testez vos connaissances en un swipe</Text>
+        <Image source={require('../assets/logo_fond.png')} style={styles.bgTop}/>
+        <Image source={require('../assets/logoMed.png')} style={styles.image}/>
+        <Text style={styles.paragraphe}>Testez vos connaissances en un swipe</Text>
       </LinearGradient>
     </View>
   );
@@ -29,6 +28,17 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  bgTop: {
+    backgroundSize: 'cover',
+    borderWidth: 1,
+    // borderColor: 'red',
+    position: 'absolute',
+    top: 10,
+    height:150,
+    zIndex: 1,
+    width: '100%',
+    // backgroundPosition: 'bottom',
+  },
   linear: {
     flex: 1,
     width: '100%',
@@ -36,11 +46,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image_fond: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-  },
   image: {
+    marginTop: 50,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
