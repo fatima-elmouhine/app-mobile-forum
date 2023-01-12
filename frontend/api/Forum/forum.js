f994bc16ee47d5aebc99e38056ff068ff8c44ec2
Lucien <lucien.zak@laplateforme.io>
2023-01-12 09:40:34 +0100

    correction bug git

---
+++ frontend/api/Forum/forum.js
@@ -0,0 +1,15 @@
+import axiosInstance from '../config'
+
+export const getTheme = async () => {
+    try {
+        const Data = await axiosInstance.get(`themes?include=Topic&limitInclude=3`)
+        return Data.data 
+    } catch (error) {
+        console.log(error)
+    }
+
+    // const { data } = await axiosInstance.get(
+    //     `/users/`,
+    // )
+    // return data
+}                   
