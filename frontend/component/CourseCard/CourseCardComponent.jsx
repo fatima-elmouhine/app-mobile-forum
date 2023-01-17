201adcf119cff08ce7bff8298a7952f230a4c535
Lucien <lucien.zak@laplateforme.io>
2023-01-17 14:30:36 +0100

    style homelogged

--- frontend/component/CourseCard/CourseCardComponent.jsx
+++ frontend/component/CourseCard/CourseCardComponent.jsx
@@ -1,62 +1,128 @@
-import React from 'react'
-import { StyleSheet, Text,Dimensions, View, ScrollView, ImageBackground, Linking} from 'react-native';
-import {  Card, Divider, Button } from 'react-native-paper';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Dimensions,
+  View,
+  ScrollView,
+  ImageBackground,
+  Linking,
+} from "react-native";
+import { Card, Divider, Button, Surface } from "react-native-paper";
 
 export default function CourseCardComponent(props) {
-    // console.log('COURSEEEEE',props.course.Theme.title)
-    // const windowWidth = Dimensions. get('window')
-    // console.log(windowWidth)
+  // console.log('COURSEEEEE',props.course.Theme.title)
+  // const windowWidth = Dimensions. get('window')
+  // console.log(windowWidth)
+  const themeColor = {
+    1: "red",
+    2: "yellow",
+    3: "#FF69B4",
+    4: "#FF1493",
+    5: "#DB7093",
+    6: "#C71585",
+    7: "#DA70D6",
+  };
+  console.log(themeColor[props.course.Theme.id]);
   return (
-    
-    <Card style={styles().container}>
-            <Card.Content style={styles.containerText}>
-                <Text style={styles.title}>
-                    {props.course.Theme.title}
-                </Text>
-                <Text color='pink' style={styles.title}>
-                    {props.course.title}
-                </Text>
-            <Divider style={{borderColor:'#e7e7e7' ,borderWidth:0.5, margin:10}} />
-            </Card.Content>
-            <View>
-
-                <Button icon="eye" mode='contained-tonal'    onPress={() => Linking.openURL(props.course.link)} >Voir</Button>
-            </View>
-      
-
-
-    </Card>
-  )
+    <Surface
+    elevation={3}
+      style={{
+        ...styles().cardContainer,
+        backgroundColor: themeColor[props.course.Theme.id],
+      }}
+    >
+      <View>
+        <Text style={styles().themeTitle}>{props.course.Theme.title}</Text>
+        <Text style={styles().title}>{props.course.title}</Text>
+      </View>
+      <Surface style={styles().btn} elevation={3}>
+        <Button
+            icon="download"
+            onPress={() => Linking.openURL(props.course.link)}
+        >
+            Télécharger
+        </Button>
+      </Surface>
+    </Surface>
+    // <Card style={styles().container}>
+    //   <Card.Content style={styles.containerText}>
+    //     <View
+    //       style={{
+    //         backgroundColor: "red",
+    //         flexDirection: "row",
+    //       }}
+    //     >
+    //       <View>
+    //         <Text style={styles.title}>{props.course.Theme.title}</Text>
+    //         <Text color="pink" style={styles.title}>
+    //           {props.course.title}
+    //         </Text>
+    //       </View>
+    //     </View>
+    //     <Button
+    //       icon="eye"
+    //       mode="contained-tonal"
+    //       onPress={() => Linking.openURL(props.course.link)}
+    //     >
+    //       Voir
+    //     </Button>
+    //   </Card.Content>
+    // </Card>
+  );
 }
-const styles = (bgColor) =>  StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        marginBottom: 16,
-        marginHorizontal: 16,
+const styles = (bgColor) =>
+  StyleSheet.create({
+    cardContainer: {
+      marginBottom: 16,
+      borderRadius: 10,
+      height: 96,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginHorizontal: 16,
+    },
+    themeTitle: {
+      fontWeight: "bold",
+      left: 16,
     },
     btn: {
-        // position: 'absolute',
-        // right: 0,
-        // top: 40,
-        // // flex: 1,
-        // // backgroundColor: 'white',
-        // // color: 'black',
-        // display: 'flex', 
-        // alignItems: 'center',
-    },
-
-    containerText: {
-        marginBottom: 10,
+      backgroundColor: "white",
+      borderRadius: 4,
+      right: 16,
+      
     },
     title: {
-        fontSize: 20,
-        color: 'pink',
-        fontWeight: '600',
-    },
-    description: {
-        fontSize: 15,
-    },
+        maxWidth: '80%',
+        left: 16,
+    }
+    // container: {
+    //   flexDirection: "row",
+    //   marginBottom: 16,
+    //   marginHorizontal: 16,
+    //   backgroundColor: "yellow",
+    // },
+    // btn: {
+    //   // position: 'absolute',
+    //   // right: 0,
+    //   // top: 40,
+    //   // // flex: 1,
+    //   // // backgroundColor: 'white',
+    //   // // color: 'black',
+    //   // display: 'flex',
+    //   // alignItems: 'center',
+    // },
 
-
-});
+    // containerText: {
+    //   marginBottom: 10,
+    //   flexDirection: "column",
+    // },
+    // title: {
+    //   fontSize: 20,
+    //   color: "pink",
+    //   fontWeight: "600",
+    // },
+    // description: {
+    //   fontSize: 15,
+    // },
+  });
--- frontend/pages/HomeLoggedScreen.js
+++ frontend/pages/HomeLoggedScreen.js
@@ -1,72 +1,122 @@
-import * as React from 'react';
-import { StyleSheet, Text, Image, View, ScrollView, ImageBackground, TouchableHighlight, SafeAreaView, TouchableOpacity } from 'react-native';
-import { useEffect, useContext, useState } from 'react';
-import { useFonts, Roboto_400Regular, Roboto_400Regular_Italic } from "@expo-google-fonts/roboto";
-import { ProgressBar, Avatar, MD3Colors, IconButton, Button } from 'react-native-paper';
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  ScrollView,
+  ImageBackground,
+  TouchableHighlight,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useContext, useState } from "react";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+} from "@expo-google-fonts/roboto";
+import {
+  ProgressBar,
+  Avatar,
+  MD3Colors,
+  IconButton,
+  Button,
+} from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
-import CourseCardComponent from '../component/CourseCard/CourseCardComponent';
-import { getCourses } from '../api/Courses/getCourses';
-import { UserContext } from '../context/UserContext';
-import * as SecureStore from 'expo-secure-store';
+import CourseCardComponent from "../component/CourseCard/CourseCardComponent";
+import { getCourses } from "../api/Courses/getCourses";
+import { UserContext } from "../context/UserContext";
+import * as SecureStore from "expo-secure-store";
 // import { getUser } from '../api/Users/getUser';
 
-export default function HomeLoggedScreen({navigation}) {
-
+export default function HomeLoggedScreen({ navigation }) {
   const [courses, setCourses] = useState([]);
   const { userDetails } = useContext(UserContext);
 
-  useEffect( () => {
+  useEffect(() => {
     async function getCoursesInHome() {
       const coursesReq = await getCourses();
       setCourses(coursesReq);
     }
     getCoursesInHome();
-  }, [])
+  }, []);
 
   function handlePressEdit() {
-    navigation.navigate('ProfileScreen');
+    navigation.navigate("ProfileScreen");
   }
 
   let [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_400Regular_Italic
+    Roboto_400Regular_Italic,
   });
 
-  
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Image source={require('../assets/logo_fond.png')} style={styles.bgTop}/> */}
       <LinearGradient
-        colors={['purple', '#02254F','#2D84EA']}
+        colors={["purple", "#02254F", "#2D84EA"]}
         style={styles.containerGradient}
       >
+        <Image
+          source={require("../assets/logo_fond.png")}
+          style={styles.bgTop}
+        />
         <View style={[styles.profileImgContainer]}>
-          <TouchableOpacity onPress={() => {handlePressEdit()}}>
-            <Avatar.Image size={100} source={require('../assets/img-test/image1.png')} style={styles.image} onPress={() => {handlePressEdit()}}/>
-          </TouchableOpacity>
+          <View
+            style={{
+              flexDirection: "row",
+              width: "100%",
+              justifyContent: "space-between",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => {
+                handlePressEdit();
+              }}
+            >
+              <Avatar.Image
+                size={120}
+                source={require("../assets/img-test/image1.png")}
+                style={styles.image}
+                onPress={() => {
+                  handlePressEdit();
+                }}
+              />
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                width: "60%",
+              }}
+            >
+              <Text style={styles.name}>Zak Lucien</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  position: "absolute",
+                  bottom: 0,
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <ProgressBar
+                  progress={0.5}
+                  color="#00FAAF"
+                  style={styles.progress}
+                />
+                <Text style={styles.paragraphe}>Lvl.2</Text>
+              </View>
+            </View>
+          </View>
         </View>
-        <Text style={styles.containerText}>
-            <Text style={styles.name}>Zak Lucien</Text>
-            <ProgressBar progress={0.35} color='#00FAAF' style={styles.progress} />
-            <Text style={styles.paragraphe}>Lvl.2</Text>
-        </Text>
-        <ScrollView  style={{display:'flex', flexDirection:'column'}}>
+        <Text style={styles.sectionTitle}>Mes derniers cours</Text>
+        <ScrollView style={{ display: "flex", flexDirection: "column" }}>
           <View style={styles.sectionLatestCourse}>
-            <Text style={styles.sectionTitle}>Les derniers cours</Text>
-            { courses.length !== 0 && 
-              courses.map((course,i) => {
-                return (
-                  <CourseCardComponent key={i}  course={course}/>
-                )
-              })
-            }
-            { courses.length !== 0 && 
-              courses.map((course,i) => {
-                return (
-                  <CourseCardComponent key={i}  course={course}/>
-                )
-              })
-            }
+            {courses.length !== 0 &&
+              courses.map((course, i) => {
+                return <CourseCardComponent key={i} course={course} />;
+              })}
           </View>
         </ScrollView>
       </LinearGradient>
@@ -76,24 +126,23 @@ export default function HomeLoggedScreen({navigation}) {
 
 const styles = StyleSheet.create({
   bgTop: {
-    backgroundSize: 'cover',
+    backgroundSize: "cover",
     borderWidth: 1,
     // borderColor: 'red',
-    position: 'absolute',
-    height:150,
-    zIndex: 1,
-    width: '100%',
+    position: "absolute",
+    height: 150,
+    width: "100%",
     // backgroundPosition: 'bottom',
   },
-  
+
   linearGradient: {
     backgroundImage: "url(../assets/img-test/image1.png)",
   },
 
   container: {
     // flex: 6,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     fontFamily: "Roboto_400Regular",
     // paddingLeft: 28,
     // alignItems: 'center',
@@ -102,49 +151,38 @@ const styles = StyleSheet.create({
 
   containerGradient: {
     // flex: 6,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     fontFamily: "Roboto_400Regular",
   },
 
   profileImgContainer: {
-    position: 'relative',
     marginHorizontal: 16,
     marginTop: 50,
-    width: 120,
+    justifyContent: "space-between",
+    marginBottom: 64,
   },
 
   image: {
     overflow: "hidden",
     borderWidth: 1,
-    zIndex: 1,
     borderStyle: "dashed",
     borderColor: "#50F4E1",
   },
 
-  icon: {
-    zIndex: 2,    
-    position: 'absolute',
-    right: 0,
-    bottom: 0,   
-  },
-
-  containerText: {
-    color: '#fff',
-    marginHorizontal: 16,
-    marginBottom: 16,
-  },
+  containerText: {},
 
   name: {
-    
+    fontSize: 20,
+    color: "#fff",
   },
 
   paragraphe: {
-    
+    fontSize: 15,
+    color: "#fff",
   },
 
   progress: {
-    
     width: 150,
     height: 20,
     borderRadius: 10,
@@ -152,6 +190,8 @@ const styles = StyleSheet.create({
 
   sectionTitle: {
     fontSize: 20,
-    color: '#fff',
-  }
+    marginHorizontal: 16,
+    color: "#fff",
+    marginBottom: 24,
+  },
 });
